7b51e2fa6c5db94e94a4ead6c16148c8c5d8950e
Holden Grissett <holdeng96@gmail.com>
2017-04-18 02:54:33 +0000

    pep8 compliance and documentation

--- 0x17-api/0-gather_data_from_an_API.py
+++ 0x17-api/0-gather_data_from_an_API.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 
 if __name__ == '__main__':
+    """
+    Grab the todo list for the specified user id
+    Format:
+    ./0-gather_data_from_an_API.py <user_id>
+    """
     import requests
     from sys import argv
 
@@ -9,9 +14,9 @@ if __name__ == '__main__':
     user = requests.get(url).json()
     todo_url = 'https://jsonplaceholder.typicode.com/todos?userId=' + argv[1]
     todo = requests.get(todo_url).json()
-    todo_done = len([o for o in todo if o.get('completed') == True])
+    todo_done = len([o for o in todo if o.get('completed') is True])
     print('Employee {} is done with tasks({}/{}):'.format(user.get('name'),
-                                                        todo_done,
-                                                        len(todo)))
+                                                          todo_done,
+                                                          len(todo)))
     for item in todo:
         print('\t{}'.format(item.get('title')))
