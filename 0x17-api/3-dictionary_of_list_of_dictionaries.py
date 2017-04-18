b6ec7146983a69aed628b38e43f7e1eaab6bc0be
Holden Grissett <holdeng96@gmail.com>
2017-04-18 04:13:53 +0000

    pep8 compliance

--- 0x17-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x17-api/3-dictionary_of_list_of_dictionaries.py
@@ -17,10 +17,11 @@ if __name__ == '__main__':
     for user in requests.get(user_url).json():
         task_list = []
         for item in requests.get(todo_url + str(user.get('id'))).json():
-            task_list.append('"task": "{}", "completed": "{}", "username": "{}"'
-                             .format(item.get('title'), item.get('complete'),
-                                     user.get('username')))
-        todo_json = '"' + str(user.get('id')) + '": [{' + '}, {'.join(task_list) + '}]'
+            task_list.append('"task": "{}", "completed"\
+: "{}", "username": "{}"'.format(item.get('title'), item.get('complete'),
+                                 user.get('username')))
+        todo_json = '"' + str(user.get('id')) \
+                    + '": [{' + '}, {'.join(task_list) + '}]'
         user_list.append(todo_json)
     json = '{' + ', '.join(user_list) + '}'
     with open('todo_all_employees.json', 'w') as json_file:
