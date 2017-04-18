72cf73be2aea81bbe6c9579daef368a770a8f134
Holden Grissett <holdeng96@gmail.com>
2017-04-18 05:20:55 +0000

    add space

--- 0x17-api/0-gather_data_from_an_API.py
+++ 0x17-api/0-gather_data_from_an_API.py
@@ -9,7 +9,6 @@ import requests
 from sys import argv
 
 if __name__ == '__main__':
-
     user_id = int(argv[1])
     url = 'https://jsonplaceholder.typicode.com/users/{}'.format(user_id)
     user = requests.get(url).json()
